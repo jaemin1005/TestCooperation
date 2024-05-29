57f6baecbaa5e57cebbc1c7a11037cc3402d5a47
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-29 15:59:26 +0900

    hotpix : 오타 수정

--- division.js
+++ division.js
@@ -1,6 +1,6 @@
 /** 덧셈 */
 function division(x, y){
-  console.log("덧셈의 결과는 : " + x/y);
+  console.log("나눗셈의 결과는 : " + x/y);
 }
 
 module.exports = division;
--- multiple.js
+++ multiple.js
@@ -1,5 +1,5 @@
 const multiple = (a, b) => {
-  console.log(`나눗셈의 결과는 : ${a * b}`);
+  console.log(`곱셈의 결과는 : ${a * b}`);
 }
 
 module.exports = multiple;
